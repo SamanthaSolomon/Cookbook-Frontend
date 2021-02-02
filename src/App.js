525006af79459dd8853176d84ce285e5b1085a0c
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-01 22:03:18 -0500

    first fetch

--- src/App.js
+++ src/App.js
@@ -6,9 +6,31 @@ import Form from "./Pages/Form"
 
 
 function App() {
+
+const url = "http://localhost:4000"
+
+const [books, setBooks] = React.useState([])
+
+//Get all
+const getBooks = () => {
+  fetch(url + '/api/cookbooks/')
+  .then((response) => response.json())
+  .then((data) => {
+    setBooks(data)
+  })
+}
+//once
+React.useEffect(() => {
+  getBooks()
+}, [])
+
+console.log("books", books)
   return (
     <div className="App">
-      <Display />
+      <Route exact path="/" render={(rp) => <Display {...rp}
+      books={books} 
+      />}
+      />
       <Form />
     </div>
   );
